7f69101b95987c7541fe254b0aa0c87f5457288f
Mazen <Mazenessam2011@outlook.com>
2025-02-09 20:25:30 +0200

    adding navigation

--- App.tsx
+++ App.tsx
@@ -2,31 +2,80 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Test from "./components/Test1";
 import { Colors } from "./constants/Color";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ScheduleScreen from "./screens/ScheduleScreen";
+import { font } from "./constants/Font";
 
-SplashScreen.preventAutoHideAsync();
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function ExpenseTrackerTabs() {
+  return (
+    <Tab.Navigator
+      id={undefined}
+      screenOptions={() => ({
+        headerStyle: { backgroundColor: Colors.mainColor },
+        headerTitleStyle: font.headline,
+        headerTitleAlign: "center",
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: "white" },
+        tabBarActiveTintColor: Colors.mainColor,
+        tabBarInactiveTintColor: Colors.SecondaryColor,
+        tabBarLabelStyle: [font.Caption],
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          title: "الرئيسية",
+          tabBarLabel: "الرئيسية",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Schedule"
+        component={ScheduleScreen}
+        options={{
+          title: "معاملاتى",
+          tabBarLabel: "معاملاتى",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          title: "حسابى",
+          tabBarLabel: "حسابى",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
-  // const [loaded, error] = useFonts({
-  //   "Cairo": require("./assets/fonts/CairoRegular.ttf"),
-  // });
-
-  // useEffect(() => {
-  //   if (loaded || error) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded, error]);
-
-  // if (!loaded && !error) {
-  //   return null;
-  // }
   return (
-    <View style={styles.container}>
-      <Test message={"مرحبا"} />
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <ExpenseTrackerTabs />
+      </NavigationContainer>
+    </>
   );
 }
 

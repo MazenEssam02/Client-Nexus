53fb1a6184a6c32ed7d468116a92394dbadc570e
Mazen <Mazenessam2011@outlook.com>
2025-02-10 22:56:28 +0200

    adding stack navigation

--- components/SearchBlock/SearchBlock.tsx
+++ components/SearchBlock/SearchBlock.tsx
@@ -2,14 +2,17 @@ import { StyleSheet, View, Text, Image } from "react-native";
 import { Colors } from "../../constants/Color";
 import { font } from "../../constants/Font";
 import SearchBar from "../SearchBar/SearchBar";
-
+import { useNavigation } from "@react-navigation/native";
 export default function SearchBlock() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={[font.title, styles.titleText]}>أحجز استشارة قانونية</Text>
       <SearchBar
         placeHolder={"ابحث بالتخصص, أو اسم المحامى"}
         backgroundColor={Colors.background}
+        onPress={() => navigation.navigate("Search" as never)}
+        iseditable={false}
       />
     </View>
   );

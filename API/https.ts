44fa6efec7466b64296a514159dc63253d9f20af
Mazen <Mazenessam2011@outlook.com>
2025-05-02 21:57:51 +0300

    get specialities from backend and link with search screen

--- API/https.ts
+++ API/https.ts
@@ -5,23 +5,27 @@ export const apiClient = axios.create({
 });
 apiClient.interceptors.request.use((config) => {
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJtYXplbmVzc2FtMjAxMUBvdXRsb29rLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkNsaWVudCIsImV4cCI6MTc0NjIwNTk2MSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdCJ9.Few__14m3U-LcWRV_WNQSgDkYyFvTxcS3HRUW5nExkc";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjQiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJtYXplbmVzc2FtMjAxMUBvdXRsb29rLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkNsaWVudCIsImV4cCI6MTc0NjIxNTg2OSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3QiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdCJ9.xGrJPYdd7Cl0tzRrqNtI_Rg8_yKhNfEl1HwNNMZAcXs";
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
-export const Lawyers = {
+export const ServiceProvider = {
   getAll: () => apiClient.get("/ServiceProvider"),
   getById: (id) => apiClient.get(`/ServiceProvider/${id}`),
-  auth: () =>
-    apiClient.post("/Auth/login", {
-      email: "Mazenessam2011@outlook.com",
-      password: "123456789",
-    }),
 };
 
 export const Documents = {
   getAll: () => apiClient.get("/api/Document"),
   getById: (id) => apiClient.get(`/api/Document/${id}`),
+};
+export const Specialization = {
+  getAll: () => apiClient.get("/api/Specialization/GetAllSpecializations"),
+  delete: (id) => apiClient.delete(`/api/Specialization/delete/${id}`),
+  add: ({ name, serviceProviderTypeId }) =>
+    apiClient.post("/api/Specialization", {
+      name: name,
+      serviceProviderTypeId: serviceProviderTypeId,
+    }),
 };
